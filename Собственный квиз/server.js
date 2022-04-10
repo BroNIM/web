338895a8034b55e83c6ec6e4580fb75f1dbf5938
BroNIM <91010547+BroNIM@users.noreply.github.com>
2022-04-10 19:54:51 +0700

    10.04.2022

--- Собственный квиз/server.js
+++ Собственный квиз/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const nunjucks = requie("nunjucks");
+
 
 const app = express();
 
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/templates/1p.html")
+    
 })
 
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/templates/play.html")
+})
 app.listen(port = 3000)
