1b01175761e5c13074da3adc10e01ebec9e8c258
BroNIM <91010547+BroNIM@users.noreply.github.com>
2022-05-15 19:52:30 +0700

    15.05.2022

--- Собственный квиз/server.js
+++ Собственный квиз/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const hbs = requie("hbs");
-const sq = require("sqlite3")
+const sqlite3 = require('sqlite3').verbose();
 
 
 const app = express();
@@ -14,7 +14,70 @@ app.get("/", function(req,res){
     res.sendFile(__dirname + "/templates/play.html")
 })
 
-async function getdata(){
-    let db = new sq.Database("Game.db")
+
+async function getdata(query,log,pass){
+    let dataq = {
+        auth:`SELECT * FROM login+passwords WHERE Login = ? and Passwords`
+    }
+    let db = new sqlite3.Database("login+passwords.db");
+    var promise= new Promise(function (resolve,reject){
+        db.all(dataq[query], [log,pass],function(err,row){
+            if(err){
+                reject(err);
+            } 
+            else {
+                resolve(row);
+            }
+        })
+    })
+    let result1 = await promise;
+    db.close();
+    return result1
+};
+
+async function registration(query,log,pass){
+    let dataq = {
+        reg:`INSERT INTO login+passwords (ID, Login, Passwords) VALUES (NULL, ?, ?)`,
+        search:`SELECT * FROM login+passwords WHERE Login = ?`,
+    }
+    let db = new sqlite3.Database("login+passwords.db");
+    var promise_reg = new Promise(function(resolve,reject){
+        db.all(dataq["search"], [log], function(err,row){
+            if (err) {
+                reject(err)
+            } else if (row.length > 0 ) {
+                console.log(row)
+                resolve("Такой пользователь уже есть")
+            } else {
+                db.run(dataq[query], [ log,pass],function(err){
+                    if (err){
+                        reject(err);
+                    }else {
+                        resolve("K")
+                    }
+                })
+            }
+        })
+    })
+    let result = await promise_reg;
+    db.close();
+    return result
+    
 }
+
+app.post("/auth", urlencodedParser, (req, res) => {
+    password = req.body.pass;
+    username = req.body.username;
+    body__req = "authorization";
+    getdata(body__req, password, username).then((rows) => {
+        if (rows.length > 0) {
+            req.session.user_auth = true;
+            res.redirect("player");
+        } else if (rows.length == 0) {
+            res.send('Ошибка');
+        }
+    }, (err) => {
+        console.log(err + " Такого пользователя нет");
+    });
+});
 app.listen(port = 3000)
